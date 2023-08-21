062044a11ce50137bc7329733dfb41970b1c64ff
SamIAmHarris <sam@groundcrewdev.com>
2023-08-21 16:47:09 +0200

    Setup deep linking for guardian acceptance

--- app/src/main/java/co/censo/vault/presentation/home/Screen.kt
+++ app/src/main/java/co/censo/vault/presentation/home/Screen.kt
@@ -6,7 +6,16 @@ sealed class Screen(val route: String) {
     object BIP39DetailRoute : Screen("bip_39_detail_screen") {
         const val BIP_39_NAME_ARG = "bip39_name"
     }
+
+    fun buildScreenDeepLinkUri() = "$VAULT_CUSTODY_URI${this.route}"
     companion object {
         const val START_DESTINATION_ID = 0
+
+        //Used for setting up deep linking options for composable screens
+        const val VAULT_CUSTODY_URI = "data://vault/"
+
+        const val GUARDIAN_DEEPLINK_ACCEPTANCE = "guardianDeepLinkAcceptance"
+
+        const val DL_TOKEN_KEY = "token"
     }
 }
