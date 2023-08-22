9098b2a3769f088692065ddac8797b0163c4f237
SamIAmHarris <sam@groundcrewdev.com>
2023-08-22 18:11:10 +0200

    Fix missing crypto manager for api service

--- app/src/main/java/co/censo/vault/di/AppModule.kt
+++ app/src/main/java/co/censo/vault/di/AppModule.kt
@@ -33,8 +33,8 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideApiService(): ApiService {
-        return ApiService.create()
+    fun provideApiService(cryptographyManager: CryptographyManager): ApiService {
+        return ApiService.create(cryptographyManager)
     }
 
     @Singleton
