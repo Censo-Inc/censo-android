e405cb5282cad4f0d7b2d299ac9a009a51f42990
Anton Onyshchenko <aonyshchenko@censo.co>
2023-09-19 15:41:57 +0200

    Remove phone biometry

--- owner/src/main/java/co/censo/vault/MainActivity.kt
+++ owner/src/main/java/co/censo/vault/MainActivity.kt
@@ -1,43 +1,28 @@
 package co.censo.vault
 
-import co.censo.vault.util.BiometricUtil
-import BlockingUI
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import co.censo.shared.data.Resource
 import co.censo.shared.data.storage.Storage
 import co.censo.vault.presentation.add_bip39.AddBIP39Screen
 import co.censo.vault.presentation.bip_39_detail.BIP39DetailScreen
-import co.censo.vault.presentation.components.OnLifecycleEvent
 import co.censo.vault.presentation.facetec_auth.FacetecAuthScreen
 import co.censo.vault.presentation.guardian_invitation.GuardianInvitationScreen
 import co.censo.vault.presentation.home.HomeScreen
@@ -45,9 +30,7 @@ import co.censo.vault.presentation.home.Screen
 import co.censo.vault.presentation.main.MainViewModel
 import co.censo.vault.presentation.owner_entrance.OwnerEntranceScreen
 import co.censo.vault.ui.theme.VaultTheme
-import co.censo.vault.util.BioPromptReason
 import co.censo.vault.util.TestTag
-import co.censo.vault.util.popUpToTop
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -68,47 +51,6 @@ class MainActivity : FragmentActivity() {
             val context = LocalContext.current
             val navController = rememberNavController()
 
-            val mainState = mainViewModel.state
-
-            LaunchedEffect(key1 = mainState) {
-
-                if (mainState.bioPromptTrigger is Resource.Success) {
-
-                    val promptInfo = BiometricUtil.createPromptInfo(context = context)
-
-                    val bioPrompt = BiometricUtil.createBioPrompt(
-                        fragmentActivity = this@MainActivity,
-                        onSuccess = {
-                            mainViewModel.onBiometryApproved()
-                        },
-                        onFail = {
-                            BiometricUtil.handleBioPromptOnFail(
-                                context = context,
-                                errorCode = it
-                            ) {
-                                mainViewModel.onBiometryFailed(errorCode = it)
-                            }
-                        }
-                    )
-
-                    bioPrompt.authenticate(promptInfo)
-                }
-
-                if (mainState.biometryInvalidated is Resource.Success) {
-                    Toast.makeText(
-                        context,
-                        getString(R.string.biometry_invalidated_keys_no_longer_accessible),
-                        Toast.LENGTH_LONG
-                    ).show()
-
-                    navController.navigate(Screen.HomeRoute.route) {
-                        launchSingleTop = true
-                        popUpToTop()
-                    }
-                    mainViewModel.resetBiometryInvalidated()
-                }
-            }
-
             VaultTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -120,27 +62,6 @@ class MainActivity : FragmentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     CensoNavHost(navController = navController)
-
-                    OnLifecycleEvent { _, event ->
-                        when (event) {
-                            Lifecycle.Event.ON_START
-                            -> {
-                                mainViewModel.onForeground(
-                                    BiometricUtil.checkForBiometricFeaturesOnDevice(context),
-                                )
-                            }
-
-                            else -> Unit
-                        }
-                    }
-
-                    BlockingUI(
-                        blockAppUI = mainState.blockAppUI,
-                        bioPromptTrigger = mainState.bioPromptTrigger,
-                        biometryUnavailable = mainState.tooManyAttempts,
-                        biometryStatus = mainState.biometryStatus,
-                        retry = mainViewModel::launchBlockingForegroundBiometryRetrieval
-                    )
                 }
             }
         }
