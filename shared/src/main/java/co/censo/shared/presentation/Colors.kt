90f51b591e90ec4be6c94c8b363d3c05c500ac56
SamIAmHarris <sam@groundcrewdev.com>
2023-10-12 08:56:18 -0400

    Add seed phrase review UI

--- shared/src/main/java/co/censo/shared/presentation/Colors.kt
+++ shared/src/main/java/co/censo/shared/presentation/Colors.kt
@@ -5,7 +5,7 @@ object SharedColors {
     val BrandGray = Color(0xFF9e9f9f)
     val GreyText = Color(0xFF666666)
     val LabelText = Color(0xFF757575)
-    val DividerGray = Color(0xFF979797)
+    val DividerGray = Color(0xFFdfdfdf)
     val CursorBlue = Color(0xFF3478f6)
     val InactiveSliderGrey = Color(0xFFe4e4e4)
     val BackgroundAlphaBlack = Color.Black.copy(alpha = 0.25f)
--- shared/src/main/java/co/censo/shared/presentation/components/CoreUI.kt
+++ shared/src/main/java/co/censo/shared/presentation/components/CoreUI.kt
@@ -1,22 +1,36 @@
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import co.censo.shared.R
 
 @Composable
 fun FullScreenButton(
     modifier: Modifier = Modifier,
     color: Color,
-    borderColor: Color,
-    border: Boolean,
-    contentPadding: PaddingValues,
+    borderColor: Color = Color.Transparent,
+    border: Boolean = false,
+    contentPadding: PaddingValues = PaddingValues(),
     enabled: Boolean = true,
     onClick: () -> Unit,
     content: @Composable RowScope.() -> Unit
@@ -29,10 +43,49 @@ fun FullScreenButton(
         ),
         contentPadding = contentPadding,
         border = if (border) BorderStroke(1.dp, borderColor) else null,
-        shape = RoundedCornerShape(4.dp),
+        shape = RoundedCornerShape(32.dp),
         enabled = enabled,
         onClick = onClick
     ) {
         content()
+    }
+}
+
+@Composable
+fun LearnMore(
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.clickable { onClick() },
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.info),
+            contentDescription = null,
+            tint = Color.Black
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = stringResource(R.string.learn_more),
+            color = Color.Black
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ButtonPreview() {
+    FullScreenButton(color = Color.Black, onClick = { }) {
+        Text(text = "Hello", color = Color.White)
+    }
+}
+
+
+@Preview
+@Composable
+fun LearnMorePreview() {
+    LearnMore {
+
     }
 }
