51f1c6079a027c49365fe2a3016f9ec29f7bfe22
SamIAmHarris <sam@groundcrewdev.com>
2023-10-30 15:55:10 -0400

    Remove unused features

--- owner/src/main/java/co/censo/vault/presentation/Screen.kt
+++ owner/src/main/java/co/censo/vault/presentation/Screen.kt
@@ -3,10 +3,6 @@ package co.censo.vault.presentation
 import Base58EncodedMasterPublicKey
 
 sealed class Screen(val route: String) {
-    object BIP39DetailRoute : Screen("bip_39_detail_screen") {
-        const val BIP_39_NAME_ARG = "bip39_name"
-    }
-
     object PlanSetupRoute : Screen("plan_setup_route") {
         const val WELCOME_FLOW_ARG = "welcome_flow_key"
 
@@ -17,11 +13,7 @@ sealed class Screen(val route: String) {
 
     object InitialPlanSetupRoute : Screen("initial_plan_setup_route")
 
-    object ActivateApprovers : Screen("activate_approvers_screen")
-
     object AccessSeedPhrases : Screen("access_seed_phrases")
-
-    object VaultScreen : Screen("vault_screen")
 
     object AccessApproval : Screen("access_approval")
 
