f6c78534ffa104543f8dda57c218cdfb6e3bb73d
SamIAmHarris <sam@groundcrewdev.com>
2023-11-07 14:48:05 -0500

    0.0.1 (8)

--- censo/build.gradle.kts
+++ censo/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "co.censo.censo"
         minSdk = 33
         targetSdk = 33
-        versionCode = 6
+        versionCode = 8
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
