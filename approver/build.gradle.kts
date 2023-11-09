e0172f9aa79820e052692e62efdde7877f7c28f3
SamIAmHarris <sam@groundcrewdev.com>
2023-11-09 08:13:51 -0500

    Bump version 0.0.1 (10)

--- approver/build.gradle.kts
+++ approver/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         applicationId = "co.censo.approver"
         minSdk = 33
         targetSdk = 33
-        versionCode = 9
+        versionCode = 10
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
--- censo/build.gradle.kts
+++ censo/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "co.censo.censo"
         minSdk = 33
         targetSdk = 33
-        versionCode = 9
+        versionCode = 10
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
