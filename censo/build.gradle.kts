5a041335d94bf50a780766474f130eba9ca8158e
Zakarie Ortiz <ZOrtiz42@outlook.com>
2023-11-16 13:12:26 -0500

    Bump build codes

--- censo/build.gradle.kts
+++ censo/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "co.censo.censo"
         minSdk = 33
         targetSdk = 33
-        versionCode = 18
+        versionCode = 19
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
