ac5013335d2e89af5a4c85fe8f69da9437ab983e
SamIAmHarris <sam@groundcrewdev.com>
2023-11-21 11:05:52 -0600

    0.0.1 (22)

--- approver/build.gradle.kts
+++ approver/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         applicationId = "co.censo.approver"
         minSdk = 33
         targetSdk = 33
-        versionCode = 21
+        versionCode = 22
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
