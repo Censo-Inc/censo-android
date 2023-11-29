792a4fd76eccc7914d1a835b243059c2bf1e0522
Zakarie Ortiz <ZOrtiz42@outlook.com>
2023-11-29 12:43:24 -0500

    Bump build numbers for both apps (30)

--- approver/build.gradle.kts
+++ approver/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         applicationId = "co.censo.approver"
         minSdk = 33
         targetSdk = 33
-        versionCode = 29
+        versionCode = 30
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
--- censo/build.gradle.kts
+++ censo/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "co.censo.censo"
         minSdk = 33
         targetSdk = 33
-        versionCode = 29
+        versionCode = 30
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
