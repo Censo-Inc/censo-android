8bbb0a22fb8e5b490f33d0d16c3bd6f3c7b82742
SamIAmHarris <sam@groundcrewdev.com>
2023-12-12 11:28:05 -0500

    Bump version 1.4.1 (36)

--- censo/build.gradle.kts
+++ censo/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "co.censo.censo"
         minSdk = 33
         targetSdk = 33
-        versionCode = 35
+        versionCode = 36
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
