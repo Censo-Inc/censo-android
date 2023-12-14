62aff4627185efb7240139af9f70c3e602643ac7
SamIAmHarris <sam@groundcrewdev.com>
2023-12-14 16:11:40 -0500

    Enter seed phrase should let user select generate or have their own before showing manual or paste

--- censo/src/main/java/co/censo/censo/presentation/enter_phrase/components/EnterSeedPhraseMainScreen.kt
+++ censo/src/main/java/co/censo/censo/presentation/enter_phrase/components/EnterSeedPhraseMainScreen.kt
@@ -5,9 +5,9 @@ import androidx.compose.foundation.layout.Column
 import MessageText
 import TitleText
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,7 +18,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,7 +30,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
@@ -39,11 +41,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.censo.censo.R
 import co.censo.censo.presentation.components.LanguageSelectionMenu
-import co.censo.censo.presentation.components.SeedPhraseAdded
 import co.censo.shared.util.BIP39
 import co.censo.shared.presentation.ButtonTextStyle
 import co.censo.shared.presentation.SharedColors
@@ -56,14 +58,21 @@ fun SelectSeedPhraseEntryType(
     onPasteEntrySelected: () -> Unit,
     onGenerateEntrySelected: () -> Unit
 ) {
-
     var selectedLanguage by remember { mutableStateOf(currentLanguage) }
+    var userHasOwnPhrase by remember { mutableStateOf(false) }
 
+    val title = if (userHasOwnPhrase) {
+        if (welcomeFlow) R.string.add_first_seed_phrase else R.string.add_seed_phrase
+    } else {
+        R.string.time_to_add_your_first_seed_phrase
+    }
+
+    val message =
+        if (userHasOwnPhrase) R.string.add_seed_phrase_message else R.string.generate_or_add_own_message
 
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
     val verticalSpacingHeight = screenHeight * 0.020f
-
 
     Column(modifier = Modifier.fillMaxSize()) {
         Image(
@@ -80,6 +89,21 @@ fun SelectSeedPhraseEntryType(
 
     Column(modifier = Modifier.fillMaxSize()) {
 
+        if (userHasOwnPhrase) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                IconButton(onClick = { userHasOwnPhrase = false }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Close,
+                        stringResource(R.string.back),
+                        tint = SharedColors.MainIconColor
+                    )
+                }
+            }
+        }
+
         Spacer(modifier = Modifier.weight(0.3f))
 
         Column(
@@ -89,9 +113,6 @@ fun SelectSeedPhraseEntryType(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Bottom
         ) {
-            val title =
-                if (welcomeFlow) R.string.add_first_seed_phrase else R.string.add_seed_phrase
-
             TitleText(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -106,7 +127,7 @@ fun SelectSeedPhraseEntryType(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 36.dp),
-                message = R.string.add_seed_phrase_message,
+                message = message,
                 textAlign = TextAlign.Start
             )
 
@@ -143,85 +164,146 @@ fun SelectSeedPhraseEntryType(
                 }
             )
 
-            Spacer(modifier = Modifier.height(verticalSpacingHeight))
-
-            StandardButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp),
-                onClick = {
-                    onManualEntrySelected(selectedLanguage)
-                },
-                contentPadding = PaddingValues(vertical = 12.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.manual_entry_icon),
-                        contentDescription = null,
-                        tint = SharedColors.ButtonTextBlue
-                    )
-                    Spacer(modifier = Modifier.width(screenWidth * 0.010f))
-                    Text(
-                        text = stringResource(R.string.input_seed_phrase),
-                        style = ButtonTextStyle.copy(fontSize = 20.sp, fontWeight = null)
-                    )
-                }
+            if (!userHasOwnPhrase) {
+                SelectPhraseCreation(
+                    verticalSpacingHeight = verticalSpacingHeight,
+                    screenWidth = screenWidth,
+                    onGenerateEntrySelected = onGenerateEntrySelected,
+                    onUserHasOwnPhrase = {
+                        userHasOwnPhrase = false
+                    }
+                )
+            } else {
+                UserHasOwnPhrase(
+                    verticalSpacingHeight = verticalSpacingHeight,
+                    screenWidth = screenWidth,
+                    onManualEntrySelected = {
+                        onManualEntrySelected(selectedLanguage)
+                    },
+                    onPasteEntrySelected = onPasteEntrySelected
+                )
             }
-
-            Spacer(modifier = Modifier.height(verticalSpacingHeight))
-
-            StandardButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp),
-                onClick = onPasteEntrySelected,
-                contentPadding = PaddingValues(vertical = 12.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = co.censo.shared.R.drawable.paste_phrase_icon),
-                        contentDescription = null,
-                        tint = SharedColors.ButtonTextBlue
-                    )
-                    Spacer(modifier = Modifier.width(screenWidth * 0.010f))
-                    Text(
-                        text = stringResource(R.string.paste_seed_phrase),
-                        style = ButtonTextStyle.copy(fontSize = 20.sp, fontWeight = null)
-                    )
-                }
-            }
-
-
-            Spacer(modifier = Modifier.height(verticalSpacingHeight))
-
-            StandardButton(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp),
-                onClick = onGenerateEntrySelected,
-                contentPadding = PaddingValues(vertical = 12.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = co.censo.shared.R.drawable.wand_and_stars),
-                        contentDescription = null,
-                        tint = SharedColors.ButtonTextBlue
-                    )
-                    Spacer(modifier = Modifier.width(screenWidth * 0.020f))
-                    Text(
-                        text = stringResource(R.string.generate_seed_phrase),
-                        style = ButtonTextStyle.copy(fontSize = 20.sp, fontWeight = null)
-                    )
-                }
-            }
-
-            Spacer(modifier = Modifier.height(verticalSpacingHeight))
         }
     }
+}
+
+@Composable
+fun ColumnScope.SelectPhraseCreation(
+    verticalSpacingHeight: Dp, screenWidth: Dp,
+    onGenerateEntrySelected: () -> Unit, onUserHasOwnPhrase: () -> Unit
+) {
+    Spacer(modifier = Modifier.height(verticalSpacingHeight))
+
+    StandardButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        onClick = onGenerateEntrySelected,
+        contentPadding = PaddingValues(vertical = 12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = co.censo.shared.R.drawable.wand_and_stars),
+                contentDescription = null,
+                tint = SharedColors.ButtonTextBlue
+            )
+            Spacer(modifier = Modifier.width(screenWidth * 0.020f))
+            Text(
+                text = stringResource(R.string.generate_seed_phrase),
+                style = ButtonTextStyle.copy(fontSize = 20.sp, fontWeight = null)
+            )
+        }
+    }
+
+
+    Spacer(modifier = Modifier.height(verticalSpacingHeight))
+
+    StandardButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        onClick = onUserHasOwnPhrase,
+        contentPadding = PaddingValues(vertical = 12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = co.censo.shared.R.drawable.paste_phrase_icon),
+                contentDescription = null,
+                tint = SharedColors.ButtonTextBlue
+            )
+            Spacer(modifier = Modifier.width(screenWidth * 0.020f))
+            Text(
+                text = stringResource(R.string.i_have_my_own),
+                style = ButtonTextStyle.copy(fontSize = 20.sp, fontWeight = null)
+            )
+        }
+    }
+
+    Spacer(modifier = Modifier.height(verticalSpacingHeight))
+}
+
+@Composable
+fun UserHasOwnPhrase(
+    verticalSpacingHeight: Dp, screenWidth: Dp,
+    onManualEntrySelected: () -> Unit, onPasteEntrySelected: () -> Unit
+) {
+    Spacer(modifier = Modifier.height(verticalSpacingHeight))
+
+    StandardButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        onClick = {
+            onManualEntrySelected()
+        },
+        contentPadding = PaddingValues(vertical = 12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.manual_entry_icon),
+                contentDescription = null,
+                tint = SharedColors.ButtonTextBlue
+            )
+            Spacer(modifier = Modifier.width(screenWidth * 0.010f))
+            Text(
+                text = stringResource(R.string.input_seed_phrase),
+                style = ButtonTextStyle.copy(fontSize = 20.sp, fontWeight = null)
+            )
+        }
+    }
+
+    Spacer(modifier = Modifier.height(verticalSpacingHeight))
+
+    StandardButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        onClick = onPasteEntrySelected,
+        contentPadding = PaddingValues(vertical = 12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = co.censo.shared.R.drawable.paste_phrase_icon),
+                contentDescription = null,
+                tint = SharedColors.ButtonTextBlue
+            )
+            Spacer(modifier = Modifier.width(screenWidth * 0.010f))
+            Text(
+                text = stringResource(R.string.paste_seed_phrase),
+                style = ButtonTextStyle.copy(fontSize = 20.sp, fontWeight = null)
+            )
+        }
+    }
+
+    Spacer(modifier = Modifier.height(verticalSpacingHeight))
 }
 
 @Preview(device = Devices.PIXEL_4_XL, showSystemUi = true, showBackground = true)
