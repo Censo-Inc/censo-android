db8f43040ee3dd80e4a39eea59fafa6abe4726c4
SamIAmHarris <samharris512@gmail.com>
2023-12-19 14:31:19 -0500

    Bump version 1.5.1 (44)

--- approver/build.gradle.kts
+++ approver/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         applicationId = "co.censo.approver"
         minSdk = 33
         targetSdk = 33
-        versionCode = 43
+        versionCode = 44
         versionName = "$versionNameMajor.$versionNameMinor.$versionNamePatch"
 
         signingConfig = if (signBuild) {
