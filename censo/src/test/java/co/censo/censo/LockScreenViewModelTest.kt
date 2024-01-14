e1216cce95c88d80dc70e1faec7b56ae6ac75dc7
Zakarie Ortiz <zortiz42@outlook.com>
2024-01-13 21:23:33 -0500

    Setup test cases for LockScreenViewModel

--- censo/src/test/java/co/censo/censo/LockScreenViewModelTest.kt
+++ censo/src/test/java/co/censo/censo/LockScreenViewModelTest.kt
@@ -2,6 +2,7 @@ package co.censo.censo
 
 import co.censo.censo.presentation.lock_screen.LockScreenState
 import co.censo.censo.presentation.lock_screen.LockScreenViewModel
+import co.censo.censo.test_helper.mockReadyOwnerStateWithPolicySetup
 import co.censo.shared.data.Resource
 import co.censo.shared.data.model.OwnerState
 import co.censo.shared.data.repository.OwnerRepository
@@ -10,6 +11,7 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.atLeastOnce
 import com.nhaarman.mockitokotlin2.whenever
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.FlowCollector
@@ -37,10 +39,11 @@ class LockScreenViewModelTest : BaseViewModelTest() {
     @Mock
     lateinit var ownerRepository: OwnerRepository
 
-    @Mock
-    lateinit var ownerStateFlow: MutableStateFlow<Resource<OwnerState>>
-
     private lateinit var lockScreenViewModel: LockScreenViewModel
+
+    private var ownerStateFlow =  MutableStateFlow<Resource<OwnerState>>(
+        Resource.Success(mockReadyOwnerStateWithPolicySetup)
+    )
 
     private val testDispatcher = StandardTestDispatcher()
     //endregion
@@ -52,8 +55,6 @@ class LockScreenViewModelTest : BaseViewModelTest() {
     @Before
     override fun setUp() {
         super.setUp()
-        //TODO: Add set test mode property if applicable
-
         Dispatchers.setMain(testDispatcher)
 
         lockScreenViewModel = LockScreenViewModel(
@@ -63,26 +64,62 @@ class LockScreenViewModelTest : BaseViewModelTest() {
 
     @After
     fun tearDown() {
-        //TODO: Add clear test mode property if applicable
         Dispatchers.resetMain()
     }
     //endregion
 
     //region Focused Tests
     @Test
-    fun `call onCreate then VM should start collection from global owner state flow`() = runTest {
+    fun `call onCreate with null data in global owner state then VM should set lock status to none`() = runTest {
         assertDefaultVMState()
 
-        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+        ownerStateFlow.tryEmit(Resource.Success(null))
 
-        }
-        //TODO: How do we mock out or test mutableStateFlow here?
-        // Maybe we want to use an actual implementation and not mock it out?
         lockScreenViewModel.onCreate()
 
         testScheduler.advanceUntilIdle()
 
-        Mockito.verify(ownerStateFlow, atLeastOnce()).collect {}
+        assertTrue(lockScreenViewModel.state.lockStatus == LockScreenState.LockStatus.None)
+    }
+
+    @Test
+    fun `call onCreate with unlocked access global owner state then VM should set lock status to unlocked`() = runTest {
+        assertDefaultVMState()
+
+        ownerStateFlow.tryEmit(Resource.Success(mockReadyOwnerStateWithPolicySetup))
+
+        lockScreenViewModel.onCreate()
+
+        testScheduler.advanceUntilIdle()
+
+        assertTrue(lockScreenViewModel.state.lockStatus is LockScreenState.LockStatus.Unlocked)
+
+        //Emit null owner state data and assert lock status is none
+        ownerStateFlow.tryEmit(Resource.Success(null))
+
+        testScheduler.advanceUntilIdle()
+
+        assertTrue(lockScreenViewModel.state.lockStatus is LockScreenState.LockStatus.None)
+    }
+
+    @Test
+    fun `call onCreate with locked access global owner state then VM should set lock status to unlocked`() = runTest {
+        assertDefaultVMState()
+
+        ownerStateFlow.tryEmit(Resource.Success(mockReadyOwnerStateWithPolicySetup.copy(unlockedForSeconds = null)))
+
+        lockScreenViewModel.onCreate()
+
+        testScheduler.advanceUntilIdle()
+
+        assertTrue(lockScreenViewModel.state.lockStatus is LockScreenState.LockStatus.Locked)
+
+        //Emit null owner state data and assert lock status is none
+        ownerStateFlow.tryEmit(Resource.Success(null))
+
+        testScheduler.advanceUntilIdle()
+
+        assertTrue(lockScreenViewModel.state.lockStatus is LockScreenState.LockStatus.None)
     }
     //endregion
 
