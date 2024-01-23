4bf3efa880c640596feac761059e15a6ddcf5f36
SamIAmHarris <samharris512@gmail.com>
2024-01-23 12:26:57 -0600

    Remove free trial text. Fix back button on learn more and welcome screen

--- censo/src/main/java/co/censo/censo/presentation/paywall/components/NoSubscriptionUI.kt
+++ censo/src/main/java/co/censo/censo/presentation/paywall/components/NoSubscriptionUI.kt
@@ -31,11 +31,6 @@ fun NoSubscriptionUI(
 ) {
     val priceText = offer.priceAndPeriodToUserText(LocalContext.current)
 
-    val trialText = offer.feeTrialPeriodISO8601
-        ?.let { "${Period.parse(it).days} ${stringResource(R.string.days_free_then)} " }
-        ?: ""
-
-
     PaywallBaseUI(
         userPriceText = priceText,
         onCancel = onCancel,
@@ -58,10 +53,6 @@ fun NoSubscriptionUI(
                 Text(
                     text = stringResource(R.string.continue_text),
                     style = ButtonTextStyle.copy(fontSize = 24.sp, fontWeight = FontWeight.Medium),
-                )
-                Text(
-                    text = "$trialText$priceText",
-                    style = ButtonTextStyle.copy(fontSize = 18.sp, fontWeight = FontWeight.Medium),
                 )
             }
         }
