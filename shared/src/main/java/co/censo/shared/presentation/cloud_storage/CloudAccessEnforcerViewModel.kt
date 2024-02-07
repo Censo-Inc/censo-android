cc5dc7948872b0da467daceacc26e108f8b0be15
Zakarie Ortiz <zortiz42@outlook.com>
2024-02-07 16:39:03 -0500

    Clear onAccessGranted when access is granted or enforceer disposes

--- shared/src/main/java/co/censo/shared/presentation/cloud_storage/CloudAccessEnforcerViewModel.kt
+++ shared/src/main/java/co/censo/shared/presentation/cloud_storage/CloudAccessEnforcerViewModel.kt
@@ -5,11 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import co.censo.shared.data.Resource
-import co.censo.shared.data.model.BiometryScanResultBlob
-import co.censo.shared.data.model.BiometryVerificationId
-import co.censo.shared.data.model.FacetecBiometry
 import co.censo.shared.data.repository.KeyRepository
+import co.censo.shared.util.projectLog
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -25,14 +22,17 @@ class CloudAccessEnforcerViewModel @Inject constructor(
     private var onAccessGranted: (() -> Unit)? = null
 
     fun onStart() {
+//        projectLog(message = "onStart collecting cloud access state")
         viewModelScope.launch {
             keyRepository.collectCloudAccessState {
+//                projectLog(message = "Cloud access state: $it")
                 onCloudAccessState(it)
             }
         }
     }
 
     fun onAccessGranted() {
+//        projectLog(message = "Access granted")
         keyRepository.updateCloudAccessState(CloudAccessState.AccessGranted)
     }
 
@@ -40,17 +40,26 @@ class CloudAccessEnforcerViewModel @Inject constructor(
         when (cloudAccessState) {
             CloudAccessState.Uninitialized -> {}
             is CloudAccessState.AccessRequired -> {
+//                projectLog(message = "onCloudAccessState Access required")
+//                projectLog(message = "onCloudAccessState Access required, setting local callback")
                 onAccessGranted = cloudAccessState.onAccessGranted
+//                projectLog(message = "onCloudAccessState Access required, setting enforce access")
                 state = state.copy(enforceAccess = true)
             }
             CloudAccessState.AccessGranted -> {
+//                projectLog(message = "onCloudAccessState Access granted")
                 state = state.copy(enforceAccess = false)
+//                projectLog(message = "onCloudAccessState Access granted, invoking local callback")
                 onAccessGranted?.invoke()
+//                projectLog(message = "onCloudAccessState Access granted, clearing local callback")
+                onAccessGranted = null
             }
         }
     }
 
     fun onDispose() {
+//        projectLog(message = "onDispose, CloudAccessEnforcer disposing")
+        onAccessGranted = null
         state = CloudAccessEnforcerState()
     }
 }
